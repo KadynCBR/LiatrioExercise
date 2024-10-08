a695ebf23486ace1edc7e2cc4fe81039239b945a
KadynCBR <kadyn_martinez@hotmail.com>
2024-10-07 22:43:39 -0700

    no ip specified.

--- Application/app.js
+++ Application/app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send({ "message": "My name is Kadyn", "timestamp": Date.now() })
 })
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
