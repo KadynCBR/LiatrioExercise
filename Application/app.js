3202c4fbb289297b929d0498db042b47692199f4
KadynCBR <kadyn_martinez@hotmail.com>
2024-10-07 22:40:17 -0700

    more introspection.

--- Application/app.js
+++ Application/app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send({ "message": "My name is Kadyn", "timestamp": Date.now() })
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}`)
 })
